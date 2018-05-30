a155c3a6b97bc6e907655a88e25a9a33b8481373
gabriel bar <gabriel.bar.pro@gmail.com>
2018-05-30 16:12:33 +0200

    scan service wUpdate et wDefender

--- scriptscan.ps1
+++ scriptscan.ps1
@@ -1,1 +1,35 @@
-﻿test
+﻿<#
+Scripte pour scan security
+
+
+New-Item -Name "Rapport-scan.txt" -ItemType file -Path "$PSScriptRoot"
+$Rapport= "$PSScriptRoot\Rapport-scan.txt"
+
+$windowsupdate= Get-Service -Name wuauserv |  Select-Object Status
+echo $windowsupdate.Status
+
+
+$windowsupdate = Get-Service -Name wuauserv 
+if ( $windowsupdate.Status -like "Stopped" ) { echo "Service Window update désactiver" >> $Rapport } else {echo "Service Window update activer" >> $Rapport }
+#>
+
+New-Item -Name "Rapport-$env:COMPUTERNAME.txt" -ItemType file -Path "$PSScriptRoot"
+$Rapport= "$PSScriptRoot\Rapport-$env:COMPUTERNAME.txt"
+
+$listservice = "wscsvc","wuauserv"; 
+
+foreach ( $checkservice in $listservice ) 
+    { 
+        if ( $checkservice.Status -like "Stopped" ) { 
+        echo "$checkservice est désactiver" >> $Rapport 
+            } else { 
+                    echo "$checkservice est activer" >> $Rapport 
+                    }
+    }
+
+<#
+Set-Service -Name wuauserv -Status Stopped
+#>
+
+
+
